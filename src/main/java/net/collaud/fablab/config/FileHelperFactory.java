507ebfc408ecd149bbecd152dcec13ecc3dd5640
Gaétan Collaud <gaetancollaud@gmail.com>
2014-06-24 21:13:09 +0200

    salt in config file

--- src/main/java/net/collaud/fablab/config/FileHelperFactory.java
+++ src/main/java/net/collaud/fablab/config/FileHelperFactory.java
@@ -1,5 +1,6 @@
 package net.collaud.fablab.config;
 
+import java.io.File;
 import net.collaud.fablab.common.file.AbstractFileHelperFactory;
 import net.collaud.fablab.common.file.FileHelper;
 import net.collaud.fablab.common.file.FileHelperException;
@@ -9,7 +10,8 @@ import net.collaud.fablab.common.file.FileHelperException;
  * @author gaetan
  */
 public class FileHelperFactory extends AbstractFileHelperFactory {
-	
+
+	public static final String DIR_CONFIG = "config/";
 	public static final String FILE_CONFIG = "fablab_config.properties";
 	public static final int ID_CONFIG = 1;
 
@@ -24,18 +26,26 @@ public class FileHelperFactory extends AbstractFileHelperFactory {
 
 	private FileHelperFactory() {
 	}
-	
-	public static FileHelper<ConfigFileHelper> getConfig(){
+
+	public static FileHelper<ConfigFileHelper> getConfig() {
 		return getInstance().get(ID_CONFIG);
 	}
 
 	@Override
-	protected FileHelper create(Integer key) throws FileHelperException{
-		switch(key){
+	protected FileHelper create(Integer key) throws FileHelperException {
+		String f = null;
+		switch (key) {
 			case ID_CONFIG:
-				return createPropertiesFileHelper(FILE_CONFIG);
+				f = FILE_CONFIG;
+				break;
+			default:
+				throw getUnknowFileHelperException(key);
 		}
-		throw getUnknowFileHelperException(key);
+		if (new File(f).exists()) {
+			return createPropertiesFileHelper(f);
+		} else {
+			return createPropertiesFileHelper(DIR_CONFIG + "/" + f);
+		}
 	}
 
 }
