9d93e48353559079dfb1a733df291681a74ac014
gaetancollaud <gaetancollaud@gmail.com>
2014-08-07 16:11:47 +0200

    Test with locale, doesn't work yet ... :'(

---
+++ src/main/java/net/collaud/fablab/util/LocaleBean.java
@@ -0,0 +1,31 @@
+package net.collaud.fablab.util;
+
+import java.io.Serializable;
+import java.util.Locale;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Gaétan
+ */
+@ManagedBean(name = "localeBean")
+@SessionScoped
+public class LocaleBean implements Serializable {
+
+    private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        this.locale = new Locale(language);
+    }
+
+}
